e9f5503a22b5b94503a3074f97d92cd3b1e903b0
Mridul Arora <aroram15@mills.cas.mcmaster.ca>
2021-01-19 09:52:17 -0500

    Implemented the function

---
+++ code.py
@@ -0,0 +1,16 @@
+def are_valid_groups(student_numbers,groups):
+	dict = {}
+	
+	for i in student_numbers:
+		dict[i] = 0
+
+	for i in student_numbers:
+		for j in groups:
+			if i in j:
+				dict[i] += 1	
+
+	for i in dict:
+		if dict[i] != 1
+			return False
+	return True
+		
