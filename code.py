19de5730c6b6eb31c6b64a8bf106697ff638e73f
Mridul Arora <aroram15@mills.cas.mcmaster.ca>
2021-01-19 10:43:54 -0500

    Corrected the code

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 def are_valid_groups(student_numbers,groups):
 	dict = {}
+	lis = []
 	
 	for i in student_numbers:
 		dict[i] = 0
@@ -9,8 +10,15 @@ def are_valid_groups(student_numbers,groups):
 			if i in j:
 				dict[i] += 1	
 
+	for i in groups:
+		if len(i) == 2 or len(i) == 3:
+			lis.append(True)
+
+	ans = all(lis)
+
 	for i in dict:
-		if dict[i] != 1
+		if dict[i] < 1 or ans == False
 			return False
+
 	return True
 		
