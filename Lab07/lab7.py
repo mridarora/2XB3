7a476f9057a7c8165c18826310b0808eae352ba1
mridarora <aroram15@mcmaster.ca>
2021-03-08 20:53:29 +0530

    BFS2 implemente

--- Lab07/lab7.py
+++ Lab07/lab7.py
@@ -59,3 +59,58 @@ def DFS(G, node1, node2):
                     return True
                 S.append(node)
     return False
+
+def BFS2(G, node1, node2): 
+    Q = [[node1]] 
+    t = []
+    if node1 == node2:  
+        return [node1]
+    while len(Q) != 0: 
+        p = Q.pop(0) 
+        marked = p[-1] 
+        if marked not in t: 
+            neighbour = G.adjacent_nodes(marked)
+            for n in neighbour: 
+                ans = list(p) 
+                ans.append(n) 
+                Q.append(ans) 
+                if n == node2: 
+                    return ans
+            t.append(marked) 
+    return []
+
+def DFS2(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                stack.append((next, path + [next]))
+    return list(stack)
+
+def BFS3(graph, start):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+graph = Graph(7)
+graph.add_edge(1, 3) 
+graph.add_edge(1, 2) 
+graph.add_edge(2, 4) 
+graph.add_edge(4, 6) 
+graph.add_edge(4, 3) 
+graph.add_edge(4, 5)
+graph.add_edge(3, 5) 
+# G.add_node(1)
+# G.add_node(2)
+# G.add_node(3)
+# G.add_node(4)
+# G.add_node(5)
+# G.add_node(6)
+print(BFS3(graph,3))
